9fcdc34c52adcf5d01a667cfc03bc0eca5e200d7
Matthew Oballe <oballematt@gmail.com>
2020-10-15 21:00:33 -0500

    making array with objects of questions that will be prompted to the user

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
 
+    },
+    {
+        type: "input",
+        name: "repository",
+        message: "What is your project repository name?"
+
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a description of your project"
+
+    }, {
+        type: "input",
+        name: "installation",
+        message: "Please provide intructions to install your project if needed"
+
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide instructions on how to use your project"
+
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please provide how others can contribute to your project if needed"
+
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+
+    },
 ];
 
 // function to write README file
