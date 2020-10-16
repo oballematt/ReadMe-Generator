e9f225c7d47ddc2cf33b0bfe8239835295380d41
Matthew Oballe <oballematt@gmail.com>
2020-10-15 21:05:22 -0500

    added parameters to add elements to table of contents based on user input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,22 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(userAnswer, userInfo) {
+  let tableOfContents = `## Table of Contents`
 
-`;
+  if (userAnswer.installation !== "") {
+    tableOfContents += `
+  * [Installation](#installation)`
+  }
+
+  if (userAnswer.usage !== "") {
+    tableOfContents += `
+  * [Usage](#usage)`
+  }
+
+  if (userAnswer.contribution !== "") {
+    tableOfContents += `
+  * [Contributing](#contribution)`
+  }
+
 }
 
 module.exports = generateMarkdown;
