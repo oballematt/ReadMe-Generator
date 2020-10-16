e54925ba64acc6fae753c14adef65770f36bbc7d
Matthew Oballe <oballematt@gmail.com>
2020-10-15 21:07:03 -0500

    created different sections that will hold body of readme based on user input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,6 +17,64 @@ function generateMarkdown(userAnswer, userInfo) {
   * [Contributing](#contribution)`
   }
 
+  let exampleMarkdown =
+    `# ${userAnswer.title}
+  (https://img.shields.io/github/languages/top/${userAnswer.username}/${userAnswer.repository}?style=flat&logo=appveyor) 
+  
+
+  ## Description
+
+  ${userAnswer.description}
+  
+  `
+
+  exampleMarkdown += tableOfContents
+
+  exampleMarkdown += `
+  * [License](#license)`
+
+  if (userAnswer.installation !== "") {
+
+    exampleMarkdown +=
+     
+    `
+  ## Installation
+  
+  ${userAnswer.installation}
+  
+  `
+  }
+
+  if (userAnswer.usage !== "") {
+    exampleMarkdown +=
+
+      `
+  ## Usage
+  
+  ${userAnswer.usage}
+  
+  `
+  }
+
+  if (userAnswer.contribution !== ""){
+    exampleMarkdown +=
+    `
+  ## Contributing
+  
+  ${userAnswer.contribution}
+  
+  `
+    
+  }
+
+  exampleMarkdown +=
+  `
+  ## License
+  
+  ${userAnswer.license}
+  
+  `
+
 }
 
 module.exports = generateMarkdown;
